e0c163b3bc46ac9ef5398683f86e6be80e919a33
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-02 23:35:21 +0700

    [wiki] feat : added wiki edit handler and view

--- main.go
+++ main.go
@@ -8,5 +8,6 @@ import (
 
 func main() {
 	http.HandleFunc("/view/", wiki.ViewHandler)
+	http.HandleFunc("/edit/", wiki.EditHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- wiki/handler.go
+++ wiki/handler.go
@@ -2,12 +2,38 @@ package wiki
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"text/template"
 )
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path)
 	title := r.URL.Path[len("/view/"):]
-	p, _ := LoadPage(title)
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func EditHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := wd + "/view/edit.html"
+	t, err := template.ParseFiles(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, p)
 }
