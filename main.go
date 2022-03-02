21bf90254f5b3471f0a467b5f2cbd79591bad0c4
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-03 00:35:23 +0700

    [wiki] feat : added create wiki route

--- main.go
+++ main.go
@@ -9,5 +9,8 @@ import (
 func main() {
 	http.HandleFunc("/view/", wiki.ViewHandler)
 	http.HandleFunc("/edit/", wiki.EditHandler)
+	http.HandleFunc("/save/", wiki.SaveHandler)
+	http.HandleFunc("/create/", wiki.CreateHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
