6500ece5749e67492dee063e16523fe980fdefc2
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-02 23:55:04 +0700

    [wiki] refactor : refactor code to get htmlfile and to loadHtml

--- wiki/handler.go
+++ wiki/handler.go
@@ -15,7 +15,13 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	file, err := getHtmlFile("/view/view.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	loadHtmlFile(file, w, p)
 }
 
 func EditHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,12 +31,26 @@ func EditHandler(w http.ResponseWriter, r *http.Request) {
 		p = &Page{Title: title}
 	}
 
-	wd, err := os.Getwd()
+	file, err := getHtmlFile("/view/edit.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	file := wd + "/view/edit.html"
+	loadHtmlFile(file, w, p)
+}
+
+func getHtmlFile(relPath string) (string, error) {
+	var file string
+	wd, err := os.Getwd()
+	if err != nil {
+		return file, err
+	}
+
+	file = wd + relPath
+	return file, nil
+}
+
+func loadHtmlFile(file string, w http.ResponseWriter, p *Page) {
 	t, err := template.ParseFiles(file)
 	if err != nil {
 		log.Fatal(err)
