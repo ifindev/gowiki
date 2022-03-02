41f568c003b62f759134a30c1467c153b65eca3b
Muhammad Arifin <muhammad.arifin1@ibm.com>
2022-03-03 00:34:54 +0700

    [wiki] feat : added create wiki handler and update view handler to redirect to create page

--- wiki/handler.go
+++ wiki/handler.go
@@ -13,7 +13,7 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, err := LoadPage(title)
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		http.Redirect(w, r, "/create/", http.StatusFound)
 	}
 
 	renderHtmlFile("view.html", w, p)
@@ -28,6 +28,32 @@ func EditHandler(w http.ResponseWriter, r *http.Request) {
 
 	renderHtmlFile("edit.html", w, p)
 }
+
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/create/"):]
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	renderHtmlFile("create.html", w, p)
+}
+
+func SaveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+
+	// Creating new wiki
+	if title == "" {
+		title = r.FormValue("title")
+	}
+
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.Save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+//TODO : show All wiki as a list with each item is a link to the wiki detail
 
 func renderHtmlFile(html string, w http.ResponseWriter, p *Page) {
 
